4381657288828579f583ab87338719b17100176c
sirh3e <sirh3e@protonmail.com>
2020-10-12 17:07:18 +0200

    added is number checks on all VERSION_*

--- configure.sh
+++ configure.sh
@@ -9,9 +9,25 @@ fi
 PROJECT_NAME=$( echo "$1" | tr '[a-z]' '[A-Z]' )
 
 PROJECT_VERSION_MAJOR=$2
+case "${PROJECT_VERSION_MAJOR}" in
+  ''|*[!0-9]*) echo "PROJECT_VERSION_MAJOR is not a number"; exit 1;;
+esac
+
 PROJECT_VERSION_MINOR=$3
+case "${PROJECT_VERSION_MINOR}" in
+  ''|*[!0-9]*) echo "PROJECT_VERSION_MINOR is not a number"; exit 1;;
+esac
+
 PROJECT_VERSION_PATCH=$4
+case "${PROJECT_VERSION_PATCH}" in
+  ''|*[!0-9]*) echo "PROJECT_VERSION_PATCH is not a number"; exit 1;;
+esac
+
 PROJECT_VERSION_RELEASE_CANDIDATE=$5
+case "${PROJECT_VERSION_RELEASE_CANDIDATE}" in
+  ''|*[!0-9]*) echo "PROJECT_VERSION_RELEASE_CANDIDATE is not a number"; exit 1;;
+esac
+
 
 echo \
 "VERSION_MAJOR ?= ${PROJECT_VERSION_MAJOR}
